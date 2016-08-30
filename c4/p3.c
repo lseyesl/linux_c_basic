2a176deae6d04257f9773c2a80a6e57a91aa6914
lseyesl <lseyesl@163.com>
2016-08-30 01:09:28 -0700

    c4 is over

---
+++ c4/p3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = -1;
+	if(x<0 || x>10)
+	{
+		printf("x is out of range.\n");	
+	}
+}
---
+++ c4/p4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 9,
+	    y = -1;
+	if(x>0&&y<=0)
+	{
+		printf("test failed!\n");	
+	}
+	else
+	{
+		printf("test ok!\n");	
+	}
+}
